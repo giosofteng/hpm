77c88d1857c0eca33de92ea57cb835aef7bd1cb6
Giorgi Zviadadze <giosofteng@gmail.com>
2023-10-10 12:09:39 -0400

    Simplify JSON parsing

--- main.py
+++ main.py
@@ -1,17 +1,8 @@
-import json
 import random
 import requests
-from types import SimpleNamespace
 
 
 records_cache = {}
-
-
-def parse_json(s):
-    return json.loads(
-        s,
-        object_hook=lambda d: SimpleNamespace(**d)
-    )
 
 
 def get_record(o):
@@ -21,26 +12,26 @@ def get_record(o):
         return records_cache[object_id]
 
     response = requests.get('https://collectionapi.metmuseum.org/public/collection/v1/objects/' + str(object_id))
-    records_cache[object_id] = parse_json(response.content)
+    records_cache[object_id] = response.json()
     return records_cache[object_id]
 
 
 if __name__ == '__main__':
     response = requests.get('https://collectionapi.metmuseum.org/public/collection/v1/departments')
-    departments = parse_json(response.content).departments
+    departments = response.json()['departments']
 
     for department in departments:
         response = requests.get('https://collectionapi.metmuseum.org/public/collection/v1/objects?departmentIds='
-                                + str(department.departmentId))
-        object_ids = parse_json(response.content).objectIDs
+                                + str(department['departmentId']))
+        object_ids = response.json()['objectIDs']
 
         record = get_record(object_ids)
         attempts = 0
-        while not record.isPublicDomain:
+        while not record['isPublicDomain']:
             attempts += 1
-            if attempts > 10:
+            if attempts > 1:
                 break
             print('RETRIEVED RECORD IS NOT IN PUBLIC DOMAIN - RETRYING...')
             record = get_record(object_ids)
         # ! GET CACHED VALUE
-        print(department.displayName + ': ' + (record.primaryImageSmall or 'NA'))
+        print(department['displayName'] + ': ' + (record['primaryImageSmall'] or 'NA'))
