708518915635223114956e4fbef7ba90a720b24b
Giorgi Zviadadze <giosofteng@gmail.com>
2023-10-11 23:14:25 -0400

    Refactor code

--- main.py
+++ main.py
@@ -2,27 +2,27 @@ import random
 import requests
 
 
-records_cache = {}
+API_URL = 'https://collectionapi.metmuseum.org/public/collection/v1/'
+RECORDS_CACHE = {}
 
 
 def get_record(o):
     object_id = random.choice(o)
 
-    if object_id in records_cache:
-        return records_cache[object_id]
+    if object_id in RECORDS_CACHE:
+        return RECORDS_CACHE[object_id]
 
-    response = requests.get('https://collectionapi.metmuseum.org/public/collection/v1/objects/' + str(object_id))
-    records_cache[object_id] = response.json()
-    return records_cache[object_id]
+    response = requests.get(API_URL + 'objects/' + str(object_id))
+    RECORDS_CACHE[object_id] = response.json()
+    return RECORDS_CACHE[object_id]
 
 
 if __name__ == '__main__':
-    response = requests.get('https://collectionapi.metmuseum.org/public/collection/v1/departments')
+    response = requests.get(API_URL + 'departments')
     departments = response.json()['departments']
 
     for department in departments:
-        response = requests.get('https://collectionapi.metmuseum.org/public/collection/v1/objects?departmentIds='
-                                + str(department['departmentId']))
+        response = requests.get(API_URL + 'objects?departmentIds=' + str(department['departmentId']))
         object_ids = response.json()['objectIDs']
 
         record = get_record(object_ids)
