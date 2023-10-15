82b642a5784c8b0fd3ce6d92d5d92af8d5029a1d
Giorgi Zviadadze <giosofteng@gmail.com>
2023-10-15 19:35:57 -0400

    Modify data collection

--- collector/main.py
+++ collector/main.py
@@ -1,37 +1,22 @@
-import random
 import requests
+import time
 
 
 API_URL = 'https://collectionapi.metmuseum.org/public/collection/v1/'
-RECORDS_CACHE = {}
 
 
-def get_record(o):
-    object_id = random.choice(o)
-
-    if object_id in RECORDS_CACHE:
-        return RECORDS_CACHE[object_id]
-
-    response = requests.get(API_URL + 'objects/' + str(object_id))
-    RECORDS_CACHE[object_id] = response.json()
-    return RECORDS_CACHE[object_id]
+def collect():
+    response = requests.get(API_URL + 'objects')
+    object_ids = response.json()['objectIDs']
+    for object_id in object_ids:
+        response = requests.get(API_URL + 'objects/' + str(object_id))
+        object = response.json()
+        image = object['primaryImageSmall']
+        if image:
+            print(image)
+        time.sleep(1)
+    collect()
 
 
 if __name__ == '__main__':
-    response = requests.get(API_URL + 'departments')
-    departments = response.json()['departments']
-
-    for department in departments:
-        response = requests.get(API_URL + 'objects?departmentIds=' + str(department['departmentId']))
-        object_ids = response.json()['objectIDs']
-
-        record = get_record(object_ids)
-        attempts = 0
-        while not record['isPublicDomain']:
-            attempts += 1
-            if attempts > 1:
-                break
-            print('RETRIEVED RECORD IS NOT IN PUBLIC DOMAIN - RETRYING...')
-            record = get_record(object_ids)
-        # ! GET CACHED VALUE
-        print(department['displayName'] + ': ' + (record['primaryImageSmall'] or 'NA'))
+    collect()
