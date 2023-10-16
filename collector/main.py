39fe4cb5e372c4e84d81908d436d87897e30ae3f
Giorgi Zviadadze <giosofteng@gmail.com>
2023-10-15 21:01:59 -0400

    Refactor code to use OOP

--- collector/main.py
+++ collector/main.py
@@ -2,21 +2,29 @@ import requests
 import time
 
 
-API_URL = 'https://collectionapi.metmuseum.org/public/collection/v1/'
+class DataCollector:
+    def __init__(self):
+        self._api_url = 'https://collectionapi.metmuseum.org/public/collection/v1/'
+        self._object_ids = []
 
+    def _get_object_ids(self):
+        response = requests.get(self._api_url + 'objects')
+        self._object_ids = response.json()['objectIDs']
 
-def collect():
-    response = requests.get(API_URL + 'objects')
-    object_ids = response.json()['objectIDs']
-    for object_id in object_ids:
-        response = requests.get(API_URL + 'objects/' + str(object_id))
-        object = response.json()
-        image = object['primaryImageSmall']
-        if image:
-            print(image)
-        time.sleep(1)
-    collect()
+    def start_data_collection(self):
+        self._get_object_ids()
+        for object_id in self._object_ids:
+            response = requests.get(self._api_url + 'objects/' + str(object_id))
+            data = response.json()
+            image_url = data['primaryImageSmall']
+            if image_url:
+                print(image_url)
+            else:
+                print('IMAGE IS NOT IN PUBLIC DOMAIN -- SKIPPING...')
+            time.sleep(1)
+        self.start_data_collection()
 
 
 if __name__ == '__main__':
-    collect()
+    data_collector = DataCollector()
+    data_collector.start_data_collection()
