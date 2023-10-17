fc8c132f28266fa6036ff522b6d7eb22cece08c4
Giorgi Zviadadze <giosofteng@gmail.com>
2023-10-16 21:16:00 -0400

    Clean code

--- collector/test/test_data_collector.py
+++ collector/test/test_data_collector.py
@@ -7,9 +7,6 @@ class TestDataCollector(unittest.TestCase):
     def setUp(self):
         self.data_collector = DataCollector()
 
-    # def tearDown(self):
-    #     self.data_collector = None
-
     def test_get_object_ids(self):
         self.assertEqual(0, len(self.data_collector.object_ids))
 
