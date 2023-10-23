645a7691141e31afca3add48f6b7f27624a7125e
Giorgi Zviadadze <giosofteng@gmail.com>
2023-10-22 21:59:32 -0400

    Add web app

---
+++ renderer/src/app.py
@@ -0,0 +1,23 @@
+from flask import Flask, jsonify, render_template
+import random
+
+from main import db
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+
+@app.route('/get_data')
+def get_data():
+    urls = list(db.get())
+    data = {'url': random.choices(urls)[0]['url']}
+    return jsonify(data)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ renderer/src/main.py
@@ -0,0 +1,27 @@
+import pika
+import random
+
+from db import DB
+
+
+db = DB()
+
+
+if __name__ == '__main__':
+    parameters = pika.ConnectionParameters('localhost')  # ! DEBUG
+    connection = pika.BlockingConnection(parameters)
+    channel = connection.channel()
+    channel.queue_declare('data_transformed')
+
+
+    def consume_data(channel, method, properties, body):
+        url = body.decode('UTF-8')
+        db.put(url)
+        # ! DEBUG
+        urls = list(db.get())
+        print(urls)
+        print(random.choices(urls)[0]['url'])
+
+
+    channel.basic_consume('data_transformed', consume_data, True)
+    channel.start_consuming()
