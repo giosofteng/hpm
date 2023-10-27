449d32498b708867c7ae447dd6a500511e08a6d6
Giorgi Zviadadze <giosofteng@gmail.com>
2023-10-26 21:05:26 -0400

    Fix deployment

--- renderer/src/app.py
+++ renderer/src/app.py
@@ -1,10 +1,12 @@
 from flask import Flask, jsonify, render_template
+import os
 import random
 
 from main import db
 
 
 app = Flask(__name__)
+port = int(os.environ.get('PORT', 5000))
 
 
 @app.route('/')
@@ -18,7 +20,9 @@ def get_data():
     if len(urls):
         data = {'url': random.choices(urls)[0]['url']}
         return jsonify(data)
+    else:
+        return jsonify({'url': 'https://images.metmuseum.org/CRDImages/ad/web-large/ap66.142.jpg'})
 
 
 if __name__ == '__main__':
-    app.run('0.0.0.0')
+    app.run('0.0.0.0', port)
